f154ced0601f2e0c4332d84e2cb4983dcec830b0
Sebastian Mejia G <67339434+SMejiaGa@users.noreply.github.com>
2024-09-28 20:29:29 -0500

    Fix/ adjustments for facturacion, deleted inventario

---
+++ src/Routes/TipoCostoRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const tipoCostoController = require('../Controllers/Postgres/PostgresTipoCostoController');
+
+router.post('/tipocostos', tipoCostoController.createTipoCosto);
+router.get('/tipocostos', tipoCostoController.getTipoCostos);
+router.get('/tipocostos/:id', tipoCostoController.getTipoCostoById);
+router.put('/tipocostos/:id', tipoCostoController.updateTipoCosto);
+router.delete('/tipocostos/:id', tipoCostoController.deleteTipoCosto);
+
+module.exports = router;
